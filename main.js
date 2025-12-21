a0aba4120044224ad5bae486cd81e4bda4fcea67
Varrox <willkat320@outlook.com>
2025-12-21 18:56:18 -0500

    Ascii art and quick links

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function(){main()}
+
+function main(){
+    apply_links()
+    add_ascii(star_01)
+}
--- quick-links.js
+++ quick-links.js
@@ -2,18 +2,36 @@ const links = {
     "godot" : "https://godotengine.org",
     "youtube" : "https://youtube.com/@Varrox",
     "github" : "https://github.com/varrox",
-    "bluesky" : "https://bluesky.com/"
+    "bluesky" : "https://bsky.app/profile/varrox.bsky.social",
+    "twitter" : "https://x.com/_Varrox",
+    "nonexistence" : "https://realbucketofchicken.github.io/nonexistance/",
+    "femtanyl" : "https://soundcloud.com/femtanyl"
 }
+
+const class_name = "quicklink"
 
 function apply_links()
 {
-    for(var i = 0; i < links.length; i++)
-    {
-        var elements = document.getElementsByClassName(links.keys[i])
-        for(var j = 0; j < elements.length; j++)
-        {
-            elements[j].
+    var elements = document.getElementsByClassName(class_name)
+    for(var i = 0; i < elements.length; i++){
+        change_to_link(elements[i])
+        i--
+    }
+}
+
+function change_to_link(element){
+    var link = ""
+    var classes = element.classList;
+    for(var i = 0; i < classes.length; i++){
+        if(classes[i] != class_name && Object.keys(links).includes(classes[i])){
+            link = classes[i]
+            break;
         }
     }
-    //
+    
+    var a = document.createElement("a")
+    a.href = links[link]
+    a.target = "_blank"
+    a.innerText = link
+    element.replaceWith(a)
 }
